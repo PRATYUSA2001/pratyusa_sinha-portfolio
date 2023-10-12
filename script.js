2f2847cbcc59adf3b329037633a8af82d4a95045
PRATYUSA2001 <116898550+PRATYUSA2001@users.noreply.github.com>
2023-10-12 21:10:28 +0530

    First adding file

---
+++ script.js
@@ -0,0 +1,10 @@
+var btn01=document.getElementById("btn01");
+var btn02=document.getElementById("btn02");
+btn01.addEventListener("click",()=>{
+    btn02.classList.toggle('hidden');
+});
+
+var hire=document.getElementById('hire');
+hire.addEventListener("click",()=>{
+    alert("Email: pratyusasinha2001@gmail.com")
+})
